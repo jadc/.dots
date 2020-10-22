8c9c87c724765005a6241216f47f1720205005ae
jadc <chehimj@gmail.com>
2020-10-21 23:53:42 -0600

    Wed Oct 21 23:53:42 MDT 2020

--- scripts/setup/xrandr.sh
+++ scripts/setup/xrandr.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-xrandr --output HDMI3 --mode 1920x1080 --rate 119.99 --pos 0x0 --output VGA1 --mode 1680x1050 --rate 59.95 --pos 1920x0
+# kvm
+#xrandr --output HDMI3 --mode 1920x1080 --rate 119.99 --pos 0x0 --output VGA1 --mode 1680x1050 --rate 59.95 --pos 1920x0
+
+# normal
+xrandr --output DP-4 --left-of HDMI-0
