6f01b568a7bd95cd20ab254316794e9d9505f8fc
jadc <chehimj@gmail.com>
2022-02-09 17:08:08 -0700

    20220209-170808

--- scripts/setup/backup.sh
+++ scripts/setup/backup.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-sudo /usr/bin/mount /dev/sdd2 /media/backup && \
+DRIVE='/dev/sdd2'
+MOUNT_POINT='/media/backup'
 
+if ! grep -qs "$MOUNT_POINT" /proc/mounts; then
+    echo "Mounting $DRIVE -> $MOUNT_POINT"
+    sudo /usr/bin/mount $DRIVE /media/backup
+fi
+
+/usr/bin/rsync -avhP --no-compress "/drives/data/archive" "$MOUNT_POINT"
