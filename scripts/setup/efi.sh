822e254854fae5f352c3db53b055d0f49e51dd5f
jadc <chehimj@gmail.com>
2022-03-09 01:30:23 -0700

    20220309-013023

--- scripts/setup/efi.sh
+++ scripts/setup/efi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ROOT='c0d4881e-c7d7-0644-8a9d-f0eb60a6a076'
 SWAP='82ce2fb4-a2ef-7140-876c-ea2285784302'
-FLAG='quiet loglevel=3 rd.systemd.show_status=auto nowatchdog nomce rd.udev.log_level=3 mitigations=off random.trust_cpu=on nvidia-drm.modeset=1 fsck.mode=skip libahci.ignore_sss=1 i915.modeset=1'
+FLAG='quiet loglevel=3 rd.systemd.show_status=auto nowatchdog nomce rd.udev.log_level=3 mitigations=off random.trust_cpu=on nvidia-drm.modeset=1 fsck.mode=skip libahci.ignore_sss=1 i915.modeset=1 intel_iommu=on'
 
 sudo efibootmgr \
     --disk /dev/sdb --part 1 \
