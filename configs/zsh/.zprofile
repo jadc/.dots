9ce3b0e0367745323ce0a16d9fbe339bb3ae17a1
jadc <chehimj@gmail.com>
2024-01-10 21:03:10 -0700

    im back

--- configs/zsh/.zprofile
+++ configs/zsh/.zprofile
@@ -1,4 +1,4 @@
 # Start X
-if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ] && [ ! -f "$HOME/.nox" ]; then
-  exec startx > /dev/null 2>&1
-fi
+#if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ] && [ ! -f "$HOME/.nox" ]; then
+#  exec startx > /dev/null 2>&1
+#fi
--- scripts/setup/efi.sh
+++ scripts/setup/efi.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-ROOT='afe7804d-9b26-4524-9177-5758b22d064f'
+ROOT='ccfb5fe7-02'
 
 # Kernel flags
 QUIET='quiet loglevel=3 rd.systemd.show_status=auto rd.udev.log_level=3'
 PERFORMANCE='nowatchdog nomce mitigations=off random.trust_cpu=on fsck.mode=skip libahci.ignore_sss=1'
 NVIDIA='nvidia-drm.modeset=1'
-#INTEL='i915.enable_fbc=1 i915.fastboot=1 i915.modeset=1'
+INTEL='i915.enable_fbc=1 i915.fastboot=1 i915.modeset=1'
 #KVM='intel_iommu=on vfio-pci.ids=10de:1e84,10de:10f8,10de:1ad8,10de:1ad9'
-FLAG="$QUIET $PERFORMANCE $NVIDIA"
+FLAG="$QUIET $PERFORMANCE $NVIDIA $INTEL"
 
 sudo efibootmgr \
     --disk /dev/nvme0n1 --part 1 \
-    --create --label "linux_zen" \
+    --create --label "Linux Zen" \
     --loader /vmlinuz-linux-zen --unicode \
     "root=PARTUUID=$ROOT rw initrd=\intel-ucode.img initrd=\initramfs-linux-zen.img $FLAG" --verbose
